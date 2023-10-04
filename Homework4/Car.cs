a8691e41ba7f911713042db46ead7941ce93a6b6
refokcer <refokcer@gmail.com>
2023-10-04 11:57:48 +0300

    Full Car class and tasks 1 - 6 , 10 - 11

--- Homework4/Car.cs
+++ Homework4/Car.cs
@@ -2,26 +2,20 @@ namespace Homework4;
 
 public class Car
 {
-    // Task4 ( 1 task ) Создаем необходимые поля
+    // Task4 ( 1 task ) Define class Car with fields name, color, price and const field CompanyName            
     private string name;
     private string color;
     private decimal price;
     private const string CompanyName = "Any Company";
 
-    // Task4 ( 3 task ) Создаем гетер сетер для цвета
+    // Task4 ( 3 task ) Create a property to access the color field.
     public string Color
     {
-        get
-        {
-            return color;    // возвращаем значение свойства
-        }
-        set
-        {
-            color = value;   // устанавливаем новое значение свойства
-        }
+        get => color; // возвращаем значение свойства
+        set => color = value; // устанавливаем новое значение свойства
     }
 
-    // Task4 ( 2 task ) дефолтный коструктор потому что дальше есть не дефолтный
+    // Task4 ( 2 task ) Create two constructors - default and with parameters.
     public Car()
     {
         name = "No name";
@@ -29,7 +23,7 @@ public class Car
         price = 0;
     }
 
-    // Конструктор по данным
+    // Constructor with parameters.
     public Car(string name, string color, decimal price)
     {
         this.name = name;
@@ -37,7 +31,7 @@ public class Car
         this.price = price;
     }
 
-    // Task4 ( 4 task ) Ввод всех входных данных с клавиатры
+    // Task4 ( 4 task ) Define methods: Input () - to enter data about cars from the console
     public static Car Input()
     {
         var name = MyUtilite.EnterValue<string>("Enter the name of the Car:");
@@ -46,10 +40,37 @@ public class Car
 
         return new Car(name, color, price);
     }
-    
-    // Task4 ( 5 task ) Выводим инфо про машину
+
+    // Task4 ( task 6 ) ChangePrice (double x) - to change the price by x%
+    public void ChangePrice(double x)
+    {
+        Console.WriteLine($"Last price: {price}");
+        price -= price * (decimal)x / 100;
+        Console.WriteLine($"New price: {price}");
+    }
+
+    // Task4 ( task 5 ) Print () - to output data about cars on the console
     public void Print()
     {
         Console.WriteLine($"Name: {name}, Color: {color}, Price: {price}, Company: {CompanyName}");
+    }
+
+    // Task4 ( task 10 ) Overload operator == for the class Car (cars - equal if the name and price are equal)
+    public static bool operator == (Car first, Car second)
+    {
+        return first.name == second.name && first.price == second.price;
+    }
+
+    // Необходимо переопределять их в паре 
+    public static bool operator !=(Car first, Car second)
+    {
+        return !(first == second);
+    }
+
+
+    // Task4 ( 11 task ) Override method ToString()  in the class Car, which returns a line with data about cars
+    public override string ToString()
+    {
+        return $"Name: {name}, Color: {color}, Price: {price}, Company: {CompanyName}";
     }
 }
