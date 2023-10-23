2577d21f8d0ace641cece77e7e10929a3ad18ad2
refokcer <refokcer@gmail.com>
2023-10-23 15:53:39 +0300

    Execute HMW 9_B

--- Homework9/Program.cs
+++ Homework9/Program.cs
@@ -9,7 +9,10 @@ internal static class Program
         Console.WriteLine("Task 9:");
         Task9_1.Execute();
         
-        Console.WriteLine("Homework 9::");
+        Console.WriteLine("Homework 9_A:");
         Homework9.Homework9_A.Execute();
+        
+        Console.WriteLine("Homework 9_B");
+        Homework9.Homework9_B.Execute();
     }
 }
