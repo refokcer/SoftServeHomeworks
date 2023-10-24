07b0653bbfee4203c6334499a16ddf8f2a84ed7a
refokcer <refokcer@gmail.com>
2023-10-24 10:37:03 +0300

    Full Homework10_1

--- Homework10/Homework10_1/Homework10_1.cs
+++ Homework10/Homework10_1/Homework10_1.cs
@@ -13,7 +13,7 @@ public static class Homework10_1
         // create list of 3 triangles and write into console the information about these shapes.
         var list = new List<Triangle>()
         {
-            new Triangle(a, b ,c),
+            new Triangle(a, b ,e),
             new Triangle(a, d ,c),
             new Triangle(d, e ,b)
         };
@@ -22,13 +22,22 @@ public static class Homework10_1
             item.Print();
         
         // Print the triangle with vertex which is the closest to origin (0,0)
-        // var dist = list[0].Points[0].Distance(new Point(0, 0));
-        // var ans = 0;
-        //
-        // for (var i = 0; i < 3; i++)
-        // {
-        //     var dist1 = list[0].Points[0].Distance(new Point(0, 0));
-        // }
+        var minDist = list[0].Points[0].Distance(new Point(0, 0));
+        var ans = 0;
+        
+        for (var i = 0; i < 3; i++)
+        {
+            var points = list[i].Points;
+            foreach (var item in points)
+            {
+                var newDist = item.Distance(new Point(0, 0));
+                if (!(newDist < minDist)) continue;
+                minDist = newDist;
+                ans = i;
+            }
+        }
+        
+        Console.WriteLine($"The {ans+1} triangle has the closest point to (0, 0) with distance {minDist}");
         
     }
 }
